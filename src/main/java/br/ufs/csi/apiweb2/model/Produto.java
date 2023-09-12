46698a301066684d73dbe483acf1e6b7b45ced91
Deborablara <debora.blara19@gmail.com>
2023-09-12 14:32:35 -0300

    criação do model de produto

---
+++ src/main/java/br/ufs/csi/apiweb2/model/Produto.java
@@ -0,0 +1,55 @@
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+  private String descricao;
+  private Boolean ativo;
+
+  public Produto() {
+  }
+
+  public Produto(String nome, String descricao, Boolean ativo) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.ativo = ativo;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public Boolean getAtivo() {
+    return ativo;
+  }
+
+  public void setAtivo(Boolean ativo) {
+    this.ativo = ativo;
+  }
+
+}
