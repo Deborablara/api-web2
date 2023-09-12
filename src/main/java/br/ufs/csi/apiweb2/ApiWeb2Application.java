aa3d7d2f3cd1bd7612ecc1e11c690eb410e0fd7b
Aluno <Aluno@politecnico.local>
2023-09-12 14:02:06 -0300

    estrutura inicial

---
+++ src/main/java/br/ufs/csi/apiweb2/ApiWeb2Application.java
@@ -0,0 +1,13 @@
+package br.ufs.csi.apiweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWeb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufs/csi/apiweb2/ApiWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufs.csi.apiweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
